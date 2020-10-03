b688ef88fd52f331b1ef1a7361cdac1151166201
kraxarn <me@kraxarn.com>
2020-10-03 17:21:51 +0200

    Initial custom style

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,11 +3,20 @@
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
+	// Custom dark theme
+	QFile styleFile(":/res/style/Dark.qss");
+	styleFile.open(QIODevice::ReadOnly);
+	setStyleSheet(styleFile.readAll());
+	styleFile.close();
+
+	// Window properties
 	setWindowIcon(Icon::get("logo:app"));
 	resize(1280, 720);
 
+	// Media player stuff
 	mediaPlayer = new QMediaPlayer(this);
 	videoWidget = new QVideoWidget(this);
 
+	// Finish setup
 	setCentralWidget(videoWidget);
 }
--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -5,6 +5,7 @@
 #include <QMainWindow>
 #include <QMediaPlayer>
 #include <QVideoWidget>
+#include <QFile>
 
 class MainWindow : public QMainWindow
 {
